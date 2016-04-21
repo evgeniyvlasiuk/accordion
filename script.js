d9e89e6fdb3278275267d03f3cd8e824764e3471
evgeniyvlasiuk <vlasiuk.zp@gmail.com>
2016-04-21 12:50:56 +0300

    accordion

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+ var a
  $(document).ready(function()  {
             $('.name').click(function(){
+            	if ($(this).attr('id') != a){
             $(".plus").css('background', 'url(play.png');
             $('.name').next('.content').slideUp();
             $(this).next('.content').slideDown();
-            var a = $(this).attr('id');
+            a = $(this).attr('id');
             var b = "#plus" + a;
             console.log(b);
             $("#plus" + a).css('background', 'url(1.png)');
-            
+           } 
     });
 });
